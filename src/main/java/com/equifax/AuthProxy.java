9d9b57af25e0b7dc04b603029227006231e3c2ec
Vijay Akkineni <akkineni.vijay@gmail.com>
2022-09-26 22:18:53 -0400

    Init commit

---
+++ src/main/java/com/equifax/AuthProxy.java
@@ -0,0 +1,14 @@
+package com.equifax;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author vijayakkineni
+ */
+@SpringBootApplication
+public class AuthProxy {
+    public static void main(String[] args) {
+        SpringApplication.run(AuthProxy.class, args);
+    }
+}
