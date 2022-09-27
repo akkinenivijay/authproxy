9ca6bb8c791b6ffdffc4e8212550ddcfe9f7c0fc
Vijay Akkineni <akkineni.vijay@gmail.com>
2022-09-27 00:10:07 -0400

    metadata xml getting generated

--- src/main/java/com/equifax/Main.java
+++
@@ -1,7 +0,0 @@
-package com.equifax;
-
-public class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello world!");
-  }
-}
--- src/main/java/com/equifax/SecurityConfiguration.java
+++ src/main/java/com/equifax/SecurityConfiguration.java
@@ -1,9 +1,17 @@
 package com.equifax;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.saml2.provider.service.metadata.OpenSamlMetadataResolver;
+import org.springframework.security.saml2.provider.service.registration.RelyingPartyRegistrationRepository;
+import org.springframework.security.saml2.provider.service.servlet.filter.Saml2WebSsoAuthenticationFilter;
+import org.springframework.security.saml2.provider.service.web.DefaultRelyingPartyRegistrationResolver;
+import org.springframework.security.saml2.provider.service.web.RelyingPartyRegistrationResolver;
+import org.springframework.security.saml2.provider.service.web.Saml2MetadataFilter;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -13,11 +21,25 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @Configuration
 public class SecurityConfiguration {
 
+  @Autowired
+  private RelyingPartyRegistrationRepository relyingPartyRegistrationRepository;
+
   @Bean
   SecurityFilterChain app(HttpSecurity http) throws Exception {
+
+    RelyingPartyRegistrationResolver relyingPartyRegistrationResolver =
+            new DefaultRelyingPartyRegistrationResolver(this.relyingPartyRegistrationRepository);
+
+    Saml2MetadataFilter filter = new Saml2MetadataFilter(
+            relyingPartyRegistrationResolver,
+            new OpenSamlMetadataResolver());
+
+    filter.setRequestMatcher(new AntPathRequestMatcher("/saml2/metadata/{registrationId}", "GET"));
+
     http.authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated())
-        .saml2Login(withDefaults())
-        .saml2Logout(withDefaults());
+            .saml2Login(withDefaults())
+            .addFilterBefore(filter, Saml2WebSsoAuthenticationFilter.class)
+            .saml2Logout(withDefaults());
     return http.build();
   }
 }
